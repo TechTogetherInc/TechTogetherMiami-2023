5218f57923f2aef92c5cb31030c70f8477ae6a57
ZackarySantana <zackzackyack@gmail.com>
2023-03-23 22:31:17 -0400

    Side-by-side sponsors

--- src/sections/sponsors.tsx
+++ src/sections/sponsors.tsx
@@ -6,17 +6,19 @@ import MLH from "../assets/mlh.png";
 export default function Sponsors() {
     return (
         <section
-            className="min-h-screen bg-gradient-to-b from-[#4c0d4d] to-[#f22d89] flex flex-col items-center \
-                            text-white sm:px-40 px-10 py-20 gap-5 justify-between">
+            className="bg-gradient-to-b from-[#4c0d4d] to-[#f22d89] flex flex-col items-center \
+                            text-white sm:px-40 px-10 py-20 gap-5">
             <h1 className="text-4xl font-semibold my-4">Our Sponsors</h1>
-            {sponsorLogos.map((logo, index) => (
-                <img
-                    key={index}
-                    src={logo}
-                    className="max-h-24 sm:h-32"
-                    alt="Sponsor Logo"
-                />
-            ))}
+            <div className="h-full flex flex-row flex-wrap gap-20 justify-center">
+                {sponsorLogos.map((logo, index) => (
+                    <img
+                        key={index}
+                        src={logo}
+                        className="max-h-24 sm:h-32"
+                        alt="Sponsor Logo"
+                    />
+                ))}
+            </div>
         </section>
     );
 }
